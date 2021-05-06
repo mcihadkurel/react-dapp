bef537ed7e8bf610779ce7cd9186831dc2e23cb4
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2021-05-06 11:38:20 +0000

    Test UI

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json'
 const greeterAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
 
 function App() {
-  const [greeting, setGreetingValue] = useState()
+  const [greeting, setGreetingValue] = useState('')
 
   async function requestAccount() {
     await window.ethereum.request({ method: 'eth_requestAccounts' });
@@ -34,6 +34,7 @@ function App() {
       const signer = provider.getSigner()
       const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer)
       const transaction = await contract.setGreeting(greeting)
+      setGreetingValue('')
       await transaction.wait()
       fetchGreeting()
     }
@@ -42,7 +43,12 @@ function App() {
 
   return (
     <div className="App">
-      
+      <header className="App-header">
+        <button onClick={fetchGreeting}>Fetch Greeting</button>
+        <button onClick={setGreeting}>Set Greeting</button>
+        <input onChange={e => setGreetingValue(e.target.value)} placeholder="Set greeting" value={greeting} />
+
+      </header>
     </div>
   );
 }
